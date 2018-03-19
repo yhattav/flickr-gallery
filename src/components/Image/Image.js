836be72dc29c2591ecfb8e6fbc99e00878b38c34
yhattav <yhattav@gmail.com>
2018-03-19 23:58:23 +0200

    working on wrapping image both in target and source

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,6 +2,85 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import './Image.scss';
+import { DragSource, DropTarget } from 'react-dnd'
+import ItemTypes from '../ItemTypes'
+import { findDOMNode } from 'react-dom'
+
+const imageSource = {
+	beginDrag(props) {
+		return {
+			id: props.id,
+			index: props.index,
+		}
+	},
+}
+
+const imageTarget = {
+	hover(props, monitor, component) {
+		const dragIndex = monitor.getItem().index
+		const hoverIndex = props.index
+
+		// Don't replace items with themselves
+		if (dragIndex === hoverIndex) {
+			return
+		}
+
+		// Determine rectangle on screen
+		const hoverBoundingRect = findDOMNode(component).getBoundingClientRect()
+
+		// Get vertical middle
+		const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+		// Determine mouse position
+		const clientOffset = monitor.getClientOffset()
+
+		// Get pixels to the top
+		const hoverClientY = clientOffset.y - hoverBoundingRect.top
+
+		// Only perform the move when the mouse has crossed half of the items height
+		// When dragging downwards, only move when the cursor is below 50%
+		// When dragging upwards, only move when the cursor is above 50%
+
+		// Dragging downwards
+		if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+			return
+		}
+
+		// Dragging upwards
+		if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+			return
+		}
+
+		// Time to actually perform the action
+		props.moveCard(dragIndex, hoverIndex)
+
+		// Note: we're mutating the monitor item here!
+		// Generally it's better to avoid mutations,
+		// but it's good here for the sake of performance
+		// to avoid expensive index searches.
+		monitor.getItem().index = hoverIndex
+	},
+}
+
+
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+// @DropTarget(ItemTypes.CARD, cardTarget, connect => ({
+// 	connectDropTarget: connect.dropTarget(),
+// }))
+
+// @DragSource(ItemTypes.CARD, cardSource, (connect, monitor) => ({
+// 	connectDragSource: connect.dragSource(),
+// 	isDragging: monitor.isDragging(),
+// }))
+
+
 
 class Image extends React.Component {
   static propTypes = {
@@ -61,3 +140,8 @@ class Image extends React.Component {
 }
 
 export default Image;
+// export const MyComponentDragSource = DragSource(/* ... */)(MyComponent);
+// // ^^^ export the intermediate component
+
+// export default const MyComponentDropTarget = DropTarget(/* ... */)(MyComponentDragSource);
+// https://github.com/react-dnd/react-dnd/blob/master/examples/04%20Sortable/Simple/Card.js#L92
