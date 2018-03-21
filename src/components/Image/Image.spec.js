4a733612383de39c42f6f2c7193a5f39af988cc4
yhattav <yhattav@gmail.com>
2018-03-21 23:26:53 +0200

    working on image tests. figured how to use the decorated component of drag target and source.

--- src/components/Image/Image.spec.js
+++ src/components/Image/Image.spec.js
@@ -3,40 +3,84 @@ import {shallow} from 'enzyme';
 import sinon from 'sinon';
 import {expect} from 'chai';
 import Image from './Image.js';
+import TestUtils from 'react-dom/test-utils';
 
-describe('Image', () => {
+ describe('Image', () => {
+   const OriginalImage = Image.DecoratedComponent;
+   const identity = el => el;
+   const sampleImage = {id: '28420720169', owner: '59717246@N05', secret: 'd460443ecb', server: '4722', farm: 5};
+   let wrapper;
+   const galleryWidth = 1111;
+   const mountImage = () => {
+     return shallow(
+       <OriginalImage name='test'
+          dto={sampleImage}
+          id='0'
+          index= {0}
+          galleryWidth={galleryWidth}
+          deleteClick={(() => {})}
+          largeClick={(() => {})}
+          connectDropTarget={identity}
+          connectDragSource={identity} />,
+       {lifecycleExperimental: true, attachTo: document.createElement('div')}
+     );
+   };
 
-  const sampleImage = {id: '28420720169', owner: '59717246@N05', secret: 'd460443ecb', server: '4722', farm: 5};
+   beforeEach(() => {
+     wrapper = mountImage();
+   });
 
-  let wrapper;
-  const galleryWidth = 1111;
+   it('render 3 icons on each image', () => {
+     expect(wrapper.find('FontAwesome').length).to.equal(3);
+   });
 
-  const mountImage = () => {
-    return shallow(
-      <Image
-        dto={sampleImage}
-        id='0'
-        galleryWidth={galleryWidth}
-        deleteClick={(() => {})}
-        largeClick={(() => {})}
-        />,
-      {lifecycleExperimental: true, attachTo: document.createElement('div')}
-    );
-  };
+   it('has 1 opacity while not dragged', () => {
+    expect(wrapper.find('.image-root').prop('style')).to.deep.include({ opacity: 1})
+    //expect(div.props.style.opacity).to.eq(1);
+  //   // Obtain the reference to the component before React DnD wrapping
+  //   const OriginalImage = Image.DecoratedComponent;
+  
+  //   // Stub the React DnD connector functions with an identity function
+  //   const identity = el => el;
+  
+  //   // Render with one set of props and test
+  //   let root = TestUtils.renderIntoDocument(
+  //     <OriginalImage name='test'
+  //         dto={sampleImage}
+  //         id='0'
+  //         index= {0}
+  //         galleryWidth={galleryWidth}
+  //         deleteClick={(() => {})}
+  //         largeClick={(() => {})}
+  //         connectDropTarget={identity}
+  //         connectDragSource={identity} />
+  //   );
+  //   let div = TestUtils.findRenderedDOMComponentWithClass(root, 'image-root');
+  //   console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' +div.props.style.opacity);
+  //   expect(div.props.style.opacity).toEqual(1);
+  
+  //   // Render with another set of props and test
+  //   root = TestUtils.renderIntoDocument(
+  //     <OriginalImage name='test'
+  //                   dto={sampleImage}
+  //                   id='0'
+  //                   index= {0}
+  //                   galleryWidth={galleryWidth}
+  //                   deleteClick={(() => {})}
+  //                   largeClick={(() => {})}
+  //                   connectDropTarget={identity}
+  //                  connectDragSource={identity}
+  //                  isDragging />
+  //   );
+  //   div = TestUtils.findRenderedDOMComponentWithClass(root, 'image-root');
+  //   expect(div.props.style.opacity).toEqual(0);
+   });
 
-  beforeEach(() => {
-    wrapper = mountImage();
-  });
-
-  it('render 3 icons on each image', () => {
-    expect(wrapper.find('FontAwesome').length).to.equal(3);
-  });
-
- it('rotates in 90 deg', () => {
-    const rotate = 90;
-    wrapper.instance().rotate_Click();
-    expect(wrapper.state().rotate).to.eq(rotate);
-  });
+  it('rotates in 90 deg', () => {
+     const rotate = 90;
+     wrapper.instance().rotate_Click();
+     expect(wrapper.state().rotate).to.eq(rotate);
+   });
 
 // not sure how to check for functions that are in the props.
   // it("simulates click event for enlarge", function() {
@@ -54,10 +98,10 @@ describe('Image', () => {
   //    spy.restore();
   //  });
 
-    it('simulates click event for rotation', function() {
-    const spy = sinon.spy(Image.prototype, 'rotate_Click');
-    wrapper.find('.rotateButton').simulate('click');
-    expect(spy.called).to.be.true;
-    spy.restore();
-  });
-});
+     it('simulates click event for rotation', function() {
+     const spy = sinon.spy(OriginalImage.prototype, 'rotate_Click');
+     wrapper.find('.rotateButton').simulate('click');
+     expect(spy.called).to.be.true;
+     spy.restore();
+     });
+ });
